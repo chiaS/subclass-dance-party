371408b0a8b9ac52303b077cf5196c42bd27079a
chiaS <pretend15@gmail.com>
2014-11-03 19:58:16 -0800

    lining up

--- src/init.js
+++ src/init.js
@@ -16,18 +16,27 @@ $(document).ready(function(){
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
   });
+
+  $(".lineUpButton").on("click", function(event) {
+    for (var i = 0; i < window.dancers.length; i++) {
+   //   if (window.dancers[i] instanceOf makePrettyDancer) {
+       window.dancers[i].lineUp(10, 10);
+      //}
+    }
+  });
+
+
 });
 
--- src/prettyDancer.js
+++ src/prettyDancer.js
@@ -1,15 +1,14 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
+var makePrettyDancer = function(top, left, timeBetweenSteps){
   makeDancer.call(this, top, left, timeBetweenSteps);
-    this.$node.addClass('blinkyDancer');
-
+   this.$node.addClass('prettyDancer');
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
  // makeDancer.prototype.setPosition.bind(this);
   //this.setPosition(top, left);
 };
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinkyDancer.prototype.step = function(){
+makePrettyDancer.prototype = Object.create(makeDancer.prototype);
+makePrettyDancer.prototype.constructor = makePrettyDancer;
+makePrettyDancer.prototype.step = function(){
   // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
 
@@ -17,4 +16,12 @@ makeBlinkyDancer.prototype.step = function(){
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
+};
+
+makePrettyDancer.prototype.lineUp = function(x,y) {
+   var styleSettings = {
+    top: x,
+    left: y
+  };
+  this.$node.css(styleSettings);
 };
